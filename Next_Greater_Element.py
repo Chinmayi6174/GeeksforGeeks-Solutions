2cf73a7868891b4af03b7a357604a33f73d9f24c
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-07-01 16:07:24 +0530

    Implement next larger element algorithm using stack
    This implementation uses a stack to efficiently find the next larger element for each item in the array, iterating from the end to the beginning.

---
+++ Next_Greater_Element.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextLargerElement(self, arr):
+        # code here
+        n= len(arr)
+        stack =[]
+        ans= []
+        for i in range(n-1, -1, -1):
+            while(len(stack)!=0 and stack[-1]<= arr[i]):
+                stack.pop()
+            if(len(stack)==0):
+                ans.append(-1)
+            elif(stack[-1]>arr[i]):
+                ans.append(stack[-1])
+            stack.append(arr[i])
+        return ans[::-1]
