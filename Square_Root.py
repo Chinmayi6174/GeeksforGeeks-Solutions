55865a0ed62a6a6ab6bac0450d36de3a45885b02
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-07-02 15:41:20 +0530

    Implement binary search for floor square root

---
+++ Square_Root.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floorSqrt(self, n): 
+        low = 1
+        high = n
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if mid * mid == n:
+                return mid
+            elif mid * mid > n:
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return high
