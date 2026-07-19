893a514e3a6372182fbaa3a8c4bc75b42deefe8c
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-07-19 15:48:56 +0530

    Implement Predecessor and Successor finder in BST

---
+++ Predecessor_&_Successor.py
@@ -0,0 +1,33 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def findPreSuc(self, root, key):
+        pred=None
+        succ=None
+        curr=root
+        while curr:
+            if curr.data<key:
+                pred=curr
+                curr=curr.right
+            elif curr.data>key:
+                succ= curr
+                curr=curr.left
+            else:
+                if curr.left:
+                    temp=curr.left
+                    while temp.right:
+                        temp=temp.right
+                    pred=temp
+                if curr.right:
+                    temp=curr.right
+                    while temp.left:
+                        temp=temp.left
+                    succ=temp
+                break
+        return (pred, succ)
